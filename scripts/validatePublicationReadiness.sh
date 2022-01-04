ed1ee933ef89476d8ab0864a7a5ae1d1a69f2c73
corno <gitkraken.a@schraverus.com>
2022-01-04 10:47:14 +0100

    updated script

--- scripts/validatePublicationReadiness.sh
+++ scripts/validatePublicationReadiness.sh
@@ -2,14 +2,17 @@
 
 dir=`realpath $(dirname "$0")`
 
-#validate that everything is committed
-git diff --exit-code && \
-
 #make sure everything is pushed
 git push && \
 
+#validate that everything is committed (to make sure we're not messing with open work with updatePackage)
+git diff --exit-code && \
+
 #make sure latest packages are installed
 $dir/updatePackage.sh ./pub && \
+
+#validate that everything is committed
+git diff --exit-code && \
 
 #buildAndTest
 $dir/buildAndTest.sh
